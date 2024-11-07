428f94df0e012eca32096a919c73153efa2683a0
StephenK94 <165089624+StephenK94@users.noreply.github.com>
2024-11-07 12:59:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,17 @@ def random_joke():
 def add():
     todo = request.form["todo"]
     todos.append({"task" : todo, "done": False})
-    return redirect(url_for('add'))
+    return redirect(url_for("index))
     
 @app.route("/check/<int:index>")
 def check(index):
     todos[index]["done"] = not todos[index]["done"]
-    return redirect(url_for('add'))
+    return redirect(url_for("index))
 
 @app.route("/delete/<int:index>")
 def delete(index):
     del todos[index]
-    return redirect(url_for('add'))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
